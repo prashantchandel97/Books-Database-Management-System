b69ea03d95229d96d10aaf5b54776e1f97fd024c
prashantchandel97 <prashantchandel1097@gmail.com>
2019-02-18 18:11:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ const port = process.env.PORT || 3000;
 
 const pool = new pg.Pool(config);
 
+//working
 app.get('/', (req, res)=>{
 	pool.connect(function(err, client, done){
 		if(err){
@@ -49,69 +50,69 @@ app.get('/', (req, res)=>{
 // app.get('/', (req, res) => {
 //    res.send('Welcome to Our SCHOOL API');
 // });
-
-app.get('/pool', function (req, res) {
-    pool.connect(function(err,client,done) {
-       if(err){
-           console.log("not able to get connection "+ err);
-           res.status(400).send(err);
-       }
-       client.query('SELECT * from students' ,function(err,result) {
-          //call `done()` to release the client back to the pool
-           done();
-           if(err){
-               console.log(err);
-               res.status(400).send(err);
-           }
-           res.status(200).send(result.rows);
-       });
-    });
-});
-
-app.post('/addstudent', function(req, res) {
-  const results = [];
-  // Grab data from http request
-  const data = {
-   name : req.body.studentName,
-   age : req.body.studentAge,
-   classroom : req.body.studentClass,
-   parents : req.body.parentContact,
-   admission : req.body.admissionDate,
- }
-  // Get a Postgres client from the connection pool
-  pool.connect(function(err, client, done) {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
-    }
-    // SQL Query > Insert Data
-    client.query( 'INSERT INTO students(student_name,student_age, student_class, parent_contact, admission_date) VALUES($1,$2,$3,$4,$5) ',[data.name, data.age, data.classroom, data.parents, data.admission]);
-
-    // SQL Query > Select Data
-    // client.query('SELECT * FROM students');
-    // Stream results back one row at a time
-
-    // query.on('row', (row) => {
-    //   results.push(row);
-    // });
-    // // After all data is returned, close connection and return results
-    // query.on('end', () => {
-    //   done();
-    //   return res.json(results);
-    // });
-    client.query('SELECT * from students' ,function(err,result) {
-       //call `done()` to release the client back to the pool
-        done();
-        if(err){
-            console.log(err);
-            res.status(400).send(err);
-        }
-        res.status(200).send(result.rows);
-    });
-  });
-});
+//
+// app.get('/pool', function (req, res) {
+//     pool.connect(function(err,client,done) {
+//        if(err){
+//            console.log("not able to get connection "+ err);
+//            res.status(400).send(err);
+//        }
+//        client.query('SELECT * from students' ,function(err,result) {
+//           //call `done()` to release the client back to the pool
+//            done();
+//            if(err){
+//                console.log(err);
+//                res.status(400).send(err);
+//            }
+//            res.status(200).send(result.rows);
+//        });
+//     });
+// });
+//
+// app.post('/addstudent', function(req, res) {
+//   const results = [];
+//   // Grab data from http request
+//   const data = {
+//    name : req.body.studentName,
+//    age : req.body.studentAge,
+//    classroom : req.body.studentClass,
+//    parents : req.body.parentContact,
+//    admission : req.body.admissionDate,
+//  }
+//   // Get a Postgres client from the connection pool
+//   pool.connect(function(err, client, done) {
+//     // Handle connection errors
+//     if(err) {
+//       done();
+//       console.log(err);
+//       return res.status(500).json({success: false, data: err});
+//     }
+//     // SQL Query > Insert Data
+//     client.query( 'INSERT INTO students(student_name,student_age, student_class, parent_contact, admission_date) VALUES($1,$2,$3,$4,$5) ',[data.name, data.age, data.classroom, data.parents, data.admission]);
+//
+//     // SQL Query > Select Data
+//     // client.query('SELECT * FROM students');
+//     // Stream results back one row at a time
+//
+//     // query.on('row', (row) => {
+//     //   results.push(row);
+//     // });
+//     // // After all data is returned, close connection and return results
+//     // query.on('end', () => {
+//     //   done();
+//     //   return res.json(results);
+//     // });
+//     client.query('SELECT * from students' ,function(err,result) {
+//        //call `done()` to release the client back to the pool
+//         done();
+//         if(err){
+//             console.log(err);
+//             res.status(400).send(err);
+//         }
+//         res.status(200).send(result.rows);
+//     });
+//   });
+// });
 
 
 app.post('/search', (req, res)=>{
